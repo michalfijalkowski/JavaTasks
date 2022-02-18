fccb98013de3515fe09c7587fa10afa0d6124398
Michał Fijałkowski <michal.fijalkowski@inspeerity.com>
2022-02-18 11:46:58 +0100

    revert

--- geolocation/src/main/java/com/task/geolocation/GeolocationApplication.java
+++ geolocation/src/main/java/com/task/geolocation/GeolocationApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GeolocationApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Test3");
+		System.out.println("Test1");
 		SpringApplication.run(GeolocationApplication.class, args);
 	}
 
