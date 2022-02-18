e932a0d73c5fb04bebe995261a2740ca0371f68d
Michał Fijałkowski <michal.fijalkowski@inspeerity.com>
2022-02-18 11:27:15 +0100

    test2

--- geolocation/src/main/java/com/task/geolocation/GeolocationApplication.java
+++ geolocation/src/main/java/com/task/geolocation/GeolocationApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GeolocationApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Test1");
+		System.out.println("Test2");
 		SpringApplication.run(GeolocationApplication.class, args);
 	}
 
